d52600d0b5d897b1fe1268c03e744ea7b1c7e483
talesCPV <tales@flexibus.com.br>
2024-07-23 16:36:43 -0300

    by_script

--- commit.sh
+++ commit.sh
@@ -23,7 +23,7 @@ then
     git commit -m "by_script"
 
 #    git branch -M main
-    git remote add origin git@github.com:talesCPV/siflex3.git
+#    git remote add origin git@github.com:talesCPV/siflex3.git
     git remote set-url origin git@github.com:talesCPV/siflex3.git
 
     git push -u -f origin main
