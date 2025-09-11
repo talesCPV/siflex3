21f931ac6be5ebd8c966011b67027a9bb957fee2
talesCPV <tales@flexibus.com.br>
2025-09-11 12:01:29 -0300

    by_script

--- scripts/main.js
+++ scripts/main.js
@@ -189,3 +189,9 @@ function loadExternalScript(src,scriptId) {
 
     return myPromisse
   }
+
+  function daysBetween(date1, date2) {
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    return Math.round((Math.abs(d2.getTime() - d1.getTime())) / (1000 * 60 * 60 * 24))
+  }
