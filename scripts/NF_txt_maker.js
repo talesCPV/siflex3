8d2b1512e2d306358a3200798e0af771e46ee335
talesCPV <tales@flexibus.com.br>
2026-02-04 16:08:27 -0300

    by_script

--- scripts/NF_txt_maker.js
+++ scripts/NF_txt_maker.js
@@ -166,19 +166,19 @@ NFe.prototype.saveRules = function(){
 
 class NFs{
     constructor(rules){        
-        this.rules = rules
-        this.reloadXML()
-/*        
-        this.viewXML()     
         const day = new Date()    
-        this.date = day.getDate().toString().padStart(2,0)+'/'+(day.getMonth()+1).toString().padStart(2,0)+'/'+day.getFullYear()
-*/
+        
+        this.Ano = day.getFullYear()
+        this.Mes = day.getMonth()+1
+        this.date = day.getDate().toString().padStart(2,0)+'/'+this.Mes.toString().padStart(2,0)+'/'+this.Ano
+        this.rules = rules
+        this.makeXML(this.rules)
+        this.formatFields()
+
+
     }
 }
 
-NFs.prototype.reloadXML = function(){
-    this.makeXML(this.rules)
-}
 
 NFs.prototype.makeXML = function(arr,parent=null){
 
@@ -238,49 +238,43 @@ NFs.prototype.getRule = function(path){
     return out
 }
 
-NFs.prototype.getValue = function(path){
-    const out = this.getRule(path)
-    return out ? out.valor.def : out
-}
-
-NFs.prototype.setValue = function(path,value){
-    console.log(this.rules)
-}
-
-NFs.prototype.formatComa = function(val){
-    val = ['NaN','undefined','null'].includes(val) ? '0,00' : val
-    return val.replace('.',',')
-}
-
-NFs.prototype.import = function(grupo, obj){
-    console.log(obj)
-/*
-    for (const grupo in obj) {
-        if(NF.hasOwnProperty(grupo)){
-            for (const campo in obj[grupo]){
-                if(NF.rules[grupo].hasOwnProperty(campo)){
-                    switch(NF.rules[grupo][campo].tipo){
-                        case 'N':
-                            obj[grupo][campo] = onlyNum(obj[grupo][campo])
-                        break
-                        case 'C':
-                            obj[grupo][campo] = onlyAlpha(obj[grupo][campo])
-                        break
-                        default:
-                            obj[grupo][campo] += campo == 'dhEmi' ? 'T07:00:00-03:00' : ''
-                            obj[grupo][campo] += campo == 'dhSaiEnt' ? 'T16:00:00-03:00' : ''
-                    }
-                    NF[grupo][campo] =  obj[grupo][campo].trim()
-                }
-            }
+NFs.prototype.getTag = function(path){
+    let out = this.xmlDoc
+    path = path.trim().split('/')    
+    for(let i=0; i<path.length; i++){
+        if(path[i].length){
+            const obj = out.querySelector(path[i])
+            out = obj != undefined ? obj : out    
         }
-    }
-*/
+    }    
+    return out
+}
 
-//    this.formatFields()
+NFs.prototype.getTagValue = function(path){
+    const tag = this.getTag(path)
+    console.log(tag)
+    return tag.innerHTML
+}
+
+NFs.prototype.setTagValue = function(path,value,coma=0,dec=2){
+    const tag = this.getTag(path)
+    tag.innerHTML = coma ? this.formatComa(value,dec) : value
+}
+
+NFs.prototype.formatComa = function(val,dec=2){
+    val = ['NaN','undefined','null'].includes(val) ? 0 : Number(val)
+    return val.toFixed(dec).replace('.',',')
 }
 
 NFs.prototype.formatFields  = function(){
+
+    this.setTagValue('Ano',this.Ano)
+    this.setTagValue('Mes',this.Mes)
+    this.setTagValue('DTIni',this.date)
+    this.setTagValue('DTFin',this.date)
+
+
+/*    
     this[10].AlqIssSN_IP    = this['CONF'].AlqIssSN
     this[20].VlNFS          = this.formatComa(this[20].VlNFS.toString())
     this[20].VlDed          = this.formatComa(this[20].VlDed.toString())
@@ -293,209 +287,7 @@ NFs.prototype.formatFields  = function(){
     this[90].ValorDed       = this.formatComa(this[90].ValorDed)
     this[90].ValorIssRetTom = this.formatComa(this[90].ValorIssRetTom)
     this[90].ValTrib        = this.formatComa(this[90].ValTrib)
-}
-
-NFs.prototype.saveRules = function(){
-    const grupos = ['10','20','30','90','CONF']
-
-    for(let i=0; i<grupos.length; i++){
-        if(this.hasOwnProperty(grupos[i])){
-            for (const key in this.rules[grupos[i]]) {
-                if(this[grupos[i]].hasOwnProperty(key)){
-                    this.rules[grupos[i]][key].def = this[grupos[i]][key] == 'NaN' ? 0 : this[grupos[i]][key]
-                }
-            }
-        }
-    }
-
-    const file_rules = JSON.stringify(this.rules)
-    saveFile(file_rules,'/../config/NFs_rules.json')
-//    .then((resolve)=>{})
-}
-
-NFs.prototype.export = function(keys){
-
-    keys = keys.split(',')
-    const NFs = Object.keys(this).sort().reduce(
-        (obj, key) => {
-          obj[key] = this[key]; 
-          return obj;
-        }, 
-        {}
-      ); 
-    let out = ''
-
-    function makeLine(obj,key){
-        let line = key+'|'        
-        for(const obj_key in obj[key]){
-            line += obj[key][obj_key] +'|'
-        }
-        return line + '\n'
-    }
-
-    for (let i=0; i<keys.length; i++) {        
-        if(typeof NFs[keys[i]] === 'object' && !Array.isArray(NFe[keys[i]]) && keys[i]!='rules'){
-            out += makeLine(NFs,keys[i])
-        }
-    }
-    return out
-}
-
-NFs.prototype.exportXML = function(nfs){
-
-/*    
-    const xml = document.implementation.createDocument(null, 'Sdt_ProcessarpsIn');
-
-    xml.newTag('Sdt_ProcessarpsIn','Login')
-    xml.newTag('Login','CodigoUsuario','609612c6-c072-4818-8e89-5490482d7f8545av18ap9305acac000-ed12-o1i')
-    xml.newTag('Login','CodigoContribuinte','54e52fdb-c30b-4236-9565-b9ef226086f012av10ap0050acac398-ed15-o4i')
-
-    xml.newTag('Sdt_ProcessarpsIn','SDTRPS')
-    xml.setAtr('SDTRPS','xmlns','NFe')
-
-    xml.newTag('SDTRPS','Ano',nfs['10'].DtFin.substr(-4))
-    xml.newTag('SDTRPS','Mes',nfs['10'].DtFin.substr(-7,2))
-    xml.newTag('SDTRPS','CPFCNPJ',nfs['10'].CpfCnpj)
-    xml.newTag('SDTRPS','DTIni',nfs['10'].DtIni)
-    xml.newTag('SDTRPS','DTFin',nfs['10'].DtFin)
-    xml.newTag('SDTRPS','TipoTrib',nfs['10'].TipoTrib)
-    xml.newTag('SDTRPS','DtAdeSN')
-    xml.newTag('SDTRPS','AlqIssSN_IP')
-    xml.newTag('SDTRPS','RegApTribSN')
-    xml.newTag('SDTRPS','TribTpSusp')
-    xml.newTag('SDTRPS','TribProcSusp')
-    xml.newTag('SDTRPS','Versao',nfs['10'].Versao)
-    xml.newTag('SDTRPS','Reg20')
-    xml.newTag('Reg20','Reg20Item')
-    xml.newTag('Reg20Item','TipoNFS',nfs['20'].TipoNFS)
-    xml.newTag('Reg20Item','NumRps',nfs['20'].NumRPS)
-    xml.newTag('Reg20Item','SerRps',nfs['20'].SerRPS)
-    xml.newTag('Reg20Item','DtEmi',nfs['20'].DtEmi)
-    xml.newTag('Reg20Item','RetFonte',nfs['20'].RetFonte)
-    xml.newTag('Reg20Item','CodSrv',nfs['20'].CodSrv)
-    xml.newTag('Reg20Item','DiscrSrv',nfs['20'].DiscrSrv)
-    xml.newTag('Reg20Item','VlNFS',nfs['20'].VlNFS)
-    xml.newTag('Reg20Item','VlDed',nfs['20'].VlDed)
-    xml.newTag('Reg20Item','DiscrDed',nfs['20'].DiscrDed)
-    xml.newTag('Reg20Item','VlBasCalc',nfs['20'].VlBasCalc)
-    xml.newTag('Reg20Item','AlqIss',nfs['20'].AlqIss)
-    xml.newTag('Reg20Item','VlIss',nfs['20'].VlIss)
-    xml.newTag('Reg20Item','VlIssRet',nfs['20'].VlIssRet)
-    xml.newTag('Reg20Item','CpfCnpTom',nfs['20'].CpfCnpjTom)
-    xml.newTag('Reg20Item','RazSocTom',nfs['20'].RazSocTom)
-    xml.newTag('Reg20Item','TipoLogtom',nfs['20'].TipoLogtom)
-    xml.newTag('Reg20Item','LogTom',nfs['20'].LogTom)
-    xml.newTag('Reg20Item','NumEndTom',nfs['20'].NumEndTom)
-    xml.newTag('Reg20Item','ComplEndTom',nfs['20'].ComplEndTom)
-    xml.newTag('Reg20Item','BairroTom',nfs['20'].BairroTom)
-    xml.newTag('Reg20Item','MunTom',nfs['20'].MunTom)
-    xml.newTag('Reg20Item','SiglaUFTom',nfs['20'].SiglaUFTom)
-    xml.newTag('Reg20Item','CepTom',nfs['20'].CepTom)
-    xml.newTag('Reg20Item','Telefone',nfs['20'].Telefone)
-    xml.newTag('Reg20Item','InscricaoMunicipal',nfs['20'].IMTom)
-    xml.newTag('Reg20Item','TipoLogLocPre',nfs['20'].TipoLogLocPre)
-    xml.newTag('Reg20Item','LogLocPre',nfs['20'].LogLocPre)
-    xml.newTag('Reg20Item','NumEndLocPre',nfs['20'].NumEndLocPre)
-    xml.newTag('Reg20Item','ComplEndLocPre',nfs['20'].ComplEndLocPre)
-    xml.newTag('Reg20Item','BairroLocPre',nfs['20'].BairroLocPre)
-    xml.newTag('Reg20Item','MunLocPre',nfs['20'].MunLocPre)
-    xml.newTag('Reg20Item','SiglaUFLocpre',nfs['20'].SiglaUFLocpre)
-    xml.newTag('Reg20Item','CepLocPre',nfs['20'].CepLocPre)
-    xml.newTag('Reg20Item','Email1',nfs['20']['E-mail-1'])
-    xml.newTag('Reg20Item','Email2',nfs['20']['E-mail-2'])
-    xml.newTag('Reg20Item','Email3',nfs['20']['E-mail-3'])
-    xml.newTag('SDTRPS','Reg90')
-    xml.newTag('Reg90','QtdRegNormal',nfs['90'].QtdRegNormal)
-    xml.newTag('Reg90','ValorNFS',nfs['90'].ValorNFS)
-    xml.newTag('Reg90','ValorISS',nfs['90'].ValorISS)
-    xml.newTag('Reg90','ValorDed',nfs['90'].ValorDed)
-    xml.newTag('Reg90','ValorIssRetTom',nfs['90'].ValorIssRetTom)
-    xml.newTag('Reg90','QtdReg30',nfs['90'].QtdReg30)
-    xml.newTag('Reg90','ValorTributos',nfs['90'].ValTrib)
-//    xml.newTag('Reg90','QtdReg40',nfs['90'].aaa)
-//    xml.newTag('Reg90','QtdReg50',nfs['90'].aaa)
-*/
-    console.log(nfs)
-
-    const xml = document.implementation.createDocument(null, 'Sdt_ProcessarpsIn');
-    xml.setAtr('Sdt_ProcessarpsIn','xmlns','NFe')
-
-    console.log(xml)
-
-    xml.newTag('Sdt_ProcessarpsIn','CPFCNPJ',nfs['10'].CpfCnpj)
-    xml.newTag('Sdt_ProcessarpsIn','DTIni',nfs['10'].DtIni)
-    xml.newTag('Sdt_ProcessarpsIn','DTFin',nfs['10'].DtFin)
-    xml.newTag('Sdt_ProcessarpsIn','TipoArq',nfs['10'].TipoArq)
-    xml.newTag('Sdt_ProcessarpsIn','Versao',nfs['10'].Versao)
-    xml.newTag('Sdt_ProcessarpsIn','Reg20')
-    xml.newTag('Reg20','Reg20Item')
-    xml.newTag('Reg20Item','TipoNf',nfs['20'].TipoNf)
-    xml.newTag('Reg20Item','NumNf',nfs['CONF'].numNFS)
-    xml.newTag('Reg20Item','SerNf',nfs['CONF'].SerNf)
-    xml.newTag('Reg20Item','DtEmiNf',nfs['20'].DtEmi)
-    xml.newTag('Reg20Item','DtHrGerNf',`${nfs['20'].DtEmi} ${(new Date()).getFullTime()}:00`)
-    xml.newTag('Reg20Item','CodVernf')
-    xml.newTag('Reg20Item','NumRps',nfs['20'].NumRPS)
-    xml.newTag('Reg20Item','SerRps',nfs['20'].SerRPS)
-    xml.newTag('Reg20Item','DtEmiRps',nfs['10'].DtIni)
-    xml.newTag('Reg20Item','TipoCpfCnpjPre',nfs['CONF'].TipoCpfCnpjPre)
-    xml.newTag('Reg20Item','CpfCnpjPre',nfs['CONF'].CpfCnpjPre)
-    xml.newTag('Reg20Item','RazSocPre',nfs['CONF'].RazSocPre)
-    xml.newTag('Reg20Item','LogPre',nfs['CONF'].LogPre)
-    xml.newTag('Reg20Item','NumEndPre',nfs['CONF'].NumEndPre)
-    xml.newTag('Reg20Item','ComplEndPre',nfs['CONF'].ComplEndPre)
-    xml.newTag('Reg20Item','BairroPre',nfs['CONF'].BairroPre)
-    xml.newTag('Reg20Item','MunPre',nfs['CONF'].MunPre)
-    xml.newTag('Reg20Item','SiglaUFPre',nfs['CONF'].SiglaUFPre)
-    xml.newTag('Reg20Item','CepPre',nfs['CONF'].CepPre)
-    xml.newTag('Reg20Item','EmailPre',nfs['CONF'].EmailPre)
-    xml.newTag('Reg20Item','TipoTribPre',nfs['CONF'].TipoTribPre)
-    xml.newTag('Reg20Item','DtAdeSN',nfs['CONF'].DtAdeSN)
-    xml.newTag('Reg20Item','AlqIssSN',nfs['CONF'].AlqIssSN)
-    xml.newTag('Reg20Item','SitNf',nfs['CONF'].SitNf)
-    xml.newTag('Reg20Item','DataCncNf')
-    xml.newTag('Reg20Item','MotivoCncNf')
-    xml.newTag('Reg20Item','TipoCpfCnpjTom',nfs['CONF'].TipoCpfCnpjTom)
-
-    xml.newTag('Reg20Item','CpfCnpjTom',nfs['20'].CpfCnpjTom)
-    xml.newTag('Reg20Item','RazSocTom',nfs['20'].RazSocTom)
-    xml.newTag('Reg20Item','LogTom',nfs['20'].LogTom)
-    xml.newTag('Reg20Item','NumEndTom',nfs['20'].NumEndTom)
-    xml.newTag('Reg20Item','ComplEndTom',nfs['20'].ComplEndTom)
-    xml.newTag('Reg20Item','BairroTom',nfs['20'].BairroTom)
-    xml.newTag('Reg20Item','MunTom',nfs['20'].MunTom)
-    xml.newTag('Reg20Item','SiglaUFTom',nfs['20'].SiglaUFTom)
-    xml.newTag('Reg20Item','CepTom',nfs['20'].CepTom)
-    xml.newTag('Reg20Item','EMailTom',nfs['20']['E-mail-1'])
-    xml.newTag('Reg20Item','LogLocPre',nfs['20'].LogLocPre)
-    xml.newTag('Reg20Item','NumEndLocPre',nfs['20'].NumEndLocPre)
-    xml.newTag('Reg20Item','ComplEndLocPre',nfs['20'].ComplEndLocPre)
-    xml.newTag('Reg20Item','BairroLocPre',nfs['20'].BairroLocPre)
-    xml.newTag('Reg20Item','MunLocPre',nfs['20'].MunLocPre)
-    xml.newTag('Reg20Item','SiglaUFLocpre',nfs['20'].SiglaUFLocpre)
-    xml.newTag('Reg20Item','CepLocPre',nfs['20'].CepLocPre)
-    xml.newTag('Reg20Item','CodSrv',nfs['20'].CodSrv)
-    xml.newTag('Reg20Item','DiscrSrv',nfs['20'].DiscrSrv)    
-    xml.newTag('Reg20Item','VlNFS',nfs['20'].VlNFS)
-    xml.newTag('Reg20Item','VlDed',nfs['20'].VlDed)
-    xml.newTag('Reg20Item','DiscrDed',nfs['20'].DiscrDed)
-    xml.newTag('Reg20Item','VlBasCalc',nfs['20'].VlBasCalc)
-    xml.newTag('Reg20Item','AlqIss',nfs['20'].AlqIss)
-    xml.newTag('Reg20Item','VlIss',nfs['20'].VlIss)
-    xml.newTag('Reg20Item','VlIssRet',nfs['20'].VlIssRet)
-
-    xml.newTag('SDTNotasExport','Reg90')
-    xml.newTag('Reg90','QtdRegNormal',nfs['90'].QtdRegNormal)
-    xml.newTag('Reg90','ValorNFS',nfs['90'].ValorNFS)
-    xml.newTag('Reg90','ValorISS',nfs['90'].ValorISS)
-    xml.newTag('Reg90','ValorDed',nfs['90'].ValorDed)
-    xml.newTag('Reg90','ValorIssRetTom',nfs['90'].ValorIssRetTom)
-    xml.newTag('Reg90','QtdReg30',nfs['90'].QtdReg30)
-    xml.newTag('Reg90','ValorTributos',nfs['90'].ValTrib)
-
-    const serializer = new XMLSerializer();
-    return serializer.serializeToString(xml)
-
+*/    
 }
 
 /****** FUNÇÔES *******/
