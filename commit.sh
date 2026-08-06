0a5fda1c713aafa36aba18da438f1bf6ba4a4221
talesCPV <tales@flexibus.com.br>
2026-08-06 09:13:24 -0300

    by_script

--- commit.sh
+++ commit.sh
@@ -6,7 +6,7 @@ echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-    cp  backend/bank/certificados ~/Documentos/Flexibus/
+    cp  -r backend/bank/certificados ~/Documentos/Flexibus/
     rm -rf backend/bank/certificados
     cp ~/Documentos/SQL/siflex/*.sql sql/
 
